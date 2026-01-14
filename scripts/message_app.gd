f95b8ecad13622f33faf24b416e7f8001a3c5412
Gillvano <freegilioshouse@gmail.com>
2026-01-14 14:42:02 +0100

    Message app working on click

--- scripts/message_app.gd
+++ scripts/message_app.gd
@@ -1,19 +1,15 @@
 extends Area3D
 
+signal open_message_app
+
 @export var app_icon : Sprite3D 
 var app_opened = false
 
-
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
 		print("app clicked")
-		if not app_opened:
-			app_icon.texture = load("res://assets/sprites/MessageAppNotif.png")
-			app_opened = true
-		else:
-			app_icon.texture = load("res://assets/sprites/MessageApp.png")
-			app_opened = false
+		open_message_app.emit()
 	
 	
 	
