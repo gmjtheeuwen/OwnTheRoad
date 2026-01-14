72b69928b5a6582421ce186f2bfc376000555400
Gillvano <freegilioshouse@gmail.com>
2026-01-14 13:08:43 +0100

    Remove temporary code

--- scripts/phone_example.gd
+++ scripts/phone_example.gd
@@ -15,14 +15,3 @@ func on_mouse_enter():
 func on_mouse_exit():
 	print("Exit phone")
 	phone_area_exited.emit()
-
-func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		#var mesh:BoxMesh = message_app_mesh.mesh
-		if not app_opened:
-			message_app_icon.texture = load("res://assets/sprites/MessageAppNotif.png")
-			app_opened = true
-		else:
-			message_app_icon.texture = load("res://assets/sprites/MessageApp.png")
-			app_opened = false
