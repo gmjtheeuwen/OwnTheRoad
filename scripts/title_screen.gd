a4c06fc083c4dd2a57c997883735384f06e7994a
gmjtheeuwen <gmjtheeuwen@gmail.com>
2026-01-23 12:49:02 +0100

    Final Final touches

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -10,6 +10,7 @@ var music_tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	fade_rect.modulate.a = 1.0
 	var tween = create_tween()
 	tween.tween_property(fade_rect, "modulate:a", 0.0, 1.5)
